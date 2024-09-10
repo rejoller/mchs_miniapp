36fd9f10ca11c11a9b3b18807166466e1d9f4083
rejoller <130197750+rejoller@users.noreply.github.com>
2024-09-10 09:07:19 +0700

    1st commit

---
+++ client/src/components/App.tsx
@@ -0,0 +1,13 @@
+import Luckyboxes from "./Luckyboxes";
+
+function App() {
+
+
+  return (
+    <>
+  <h1 className="text-red-600">123444</h1>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Luckyboxes.tsx
@@ -0,0 +1,11 @@
+function Luckyboxes () {
+
+
+    return (
+      <>
+    <h1 className="text-red-600">123444</h1>
+      </>
+    )
+  }
+  
+  export default App 
